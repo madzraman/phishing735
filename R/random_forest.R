2c0514003cac3cf5d0d195418d3b95f7440484fa
Will Tang <tcw20000623@gmail.com>
2024-04-17 14:51:13 -0400

    Changed to ranger

--- R/random_forest.R
+++ R/random_forest.R
@@ -1,4 +1,5 @@
 library(caret)
+library(ranger)
 source('helpers.R')
 
 
@@ -9,8 +10,27 @@ test_x <- get_data('test_x')
 test_y <- get_data('test_y')
 
 # Run 
+
+param_grid<-expand.grid(mtry = c(10),
+                        splitrule = c("extratrees"),
+                       min.node.size = c(1, 5))
 trCtl <- trainControl(method="cv", number=5, savePredictions=F,verboseIter = T)
-fit <- train(train_x,train_y,method='rf',trControl = trCtl)
+
+models=list()
+for (ntree in c(50,500,5000)){
+    set.seed(123)
+    rf_model <- train(x = train_x,
+                      y = train_y,
+                      method = "ranger",
+                      trControl = trCtl,
+                      tuneGrid = param_grid,
+                      num.trees=ntree)
+    name=paste0(ntree,"_tr_model")
+    models[[name]]=rf_model
+}
+trCtl <- trainControl(method="cv", number=5, savePredictions=F,verboseIter = T)
+fit <- train(train_x,train_y,method='ranger',trControl = trCtl,tuneGrid = param_grid)
+fit <- readRDS('../derived_data/model.RDS')
 
 
 pred_y <- predict(fit,newdata = test_x)
